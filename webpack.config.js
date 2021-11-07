8d8f1b372990fc25c65b126188c2f65c216367ed
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-11-07 20:53:35 +0100

    chore(webpack): change to better source maps

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ console.log ('Production mode is: ', isProduction);
 module.exports = {
   'watch': !isProduction,
   'mode': isProduction ? 'production' : 'development',
-  'devtool': isProduction ? false : 'cheap-source-map',
+  'devtool': isProduction ? false : 'eval-source-map',
   'entry': {
     'scripts/content': './src/scripts/content.js',
   },
