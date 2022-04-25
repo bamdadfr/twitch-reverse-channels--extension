2f014a794c2d1178dc7d3a80c07d82d36cea9706
Bamdad Sabbagh <hi@bamdad.fr>
2022-04-25 11:58:57 +0200

    feat: Add typescript webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,25 @@
-const CopyPlugin = require ('copy-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 const isProduction = process.env.NODE_ENV === 'production';
 
 // eslint-disable-next-line no-console
-console.log ('Production mode is: ', isProduction);
+console.log('Production mode is: ', isProduction);
 
 module.exports = {
   'watch': !isProduction,
   'mode': isProduction ? 'production' : 'development',
   'devtool': isProduction ? false : 'eval-source-map',
   'entry': {
-    'scripts/content': './src/scripts/content.js',
+    'scripts/content': './src/app/content.ts',
+  },
+  'resolve': {
+    extensions: ['.ts', '.js'],
   },
   'output': {
     'publicPath': '',
   },
   'node': false,
   'plugins': [
-    new CopyPlugin ({
+    new CopyPlugin({
       'patterns': [
         {
           'from': './src/manifest.json',
@@ -32,11 +35,14 @@ module.exports = {
   'module': {
     'rules': [
       {
-        'test': /\.js$/,
-        'exclude': /node_modules/,
-        'use': {
-          'loader': 'babel-loader',
-        },
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
       },
     ],
   },
